d51fb90ff51ce8272e1a32b88f1a76835aa585eb
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2023-12-16 13:02:21 -0600

    Refactor pre-release tagging script

--- src/templates/create-nuget-package/powershell/configure-git-version/functions/set-pre-release-tag.ps1
+++ src/templates/create-nuget-package/powershell/configure-git-version/functions/set-pre-release-tag.ps1
@@ -3,7 +3,7 @@
 Updates the pre-release tag in the GitVersion configuration based on the specified Git branch.
 .DESCRIPTION
 The `Set-PreReleaseTag` function adjusts the GitVersion configuration to set the appropriate pre-release tag based on the branch name.
-For both feature and hotfix branches, it extracts the respective branch's name and uses it as the pre-release tag.
+This function is designed to handle feature, bugfix, refactor, and hotfix branches, extracting the respective branch's name (leaf) and using it as the pre-release tag.
 .PARAMETER BranchName
 The name of the Git branch for which the pre-release tag needs to be configured.
 .PARAMETER ConfigPath
@@ -12,10 +12,16 @@ Path to the GitVersion configuration file that governs versioning rules.
 Set-PreReleaseTag -BranchName "refs/heads/feature/new-feature" -ConfigPath "C:\path\to\gitversion.yml"
 Updates the GitVersion configuration file to set the pre-release tag as "new-feature" based on the provided feature branch.
 .EXAMPLE
+Set-PreReleaseTag -BranchName "refs/heads/bugfix/fix-issue" -ConfigPath "C:\path\to\gitversion.yml"
+Updates the GitVersion configuration file to set the pre-release tag as "fix-issue" based on the provided bugfix branch.
+.EXAMPLE
+Set-PreReleaseTag -BranchName "refs/heads/refactor/code-cleanup" -ConfigPath "C:\path\to\gitversion.yml"
+Updates the GitVersion configuration file to set the pre-release tag as "code-cleanup" based on the provided refactor branch.
+.EXAMPLE
 Set-PreReleaseTag -BranchName "refs/heads/hotfix/urgent-fix" -ConfigPath "C:\path\to\gitversion.yml"
 Updates the GitVersion configuration file to set the pre-release tag as "urgent-fix" based on the provided hotfix branch.
 .NOTES
-This function targets feature and hotfix branches, deriving the pre-release tag from the branch's name.
+This function targets feature, bugfix, refactor, and hotfix branches, deriving the pre-release tag from the branch's name (leaf).
 Always ensure to back up your configuration or use source control before applying modifications with this function.
 #>
 function Set-PreReleaseTag {
@@ -40,22 +46,27 @@ function Set-PreReleaseTag {
         return
     }
 
-    # Determine the branch type and extract the name
-    switch ($BranchName) {
-        { $_ -match '^refs/heads/features?/' } {  # Matches both 'feature/' and 'features/'
-            $BranchTagName = ($_ -split '/')[-1]  # Extract the leaf name
-            $Config.branches.feature.tag = $BranchTagName
-            Write-Host "Feature branch detected. Setting the pre-release tag to: $BranchTagName"
+    $BranchTypeMap = @{
+        "Feature"  = { $_ -match '^refs/heads/features?/' }
+        "Bugfix"   = { $_ -match '^refs/heads/(bugfix(es)?|fix(es)?)[/-]' }
+        "Refactor" = { $_ -match '^refs/heads/refactor(ing)?/' }
+        "Hotfix"   = { $_ -match '^refs/heads/hotfix(es)?/' }
+    }
+
+    $Matched = $false
+    foreach ($Entry in $BranchTypeMap.GetEnumerator()) {
+        if ($BranchName -match $Entry.Value.Invoke()) {
+            $BranchTagName = ($BranchName -split '/')[-1]
+            $Config.branches.$($Entry.Key).tag = $BranchTagName
+            Write-Host "$($Entry.Key.ToUpper()) branch detected. Setting the pre-release tag to: $BranchTagName"
+            $Matched = $true
+            break
         }
-        { $_ -match '^refs/heads/hotfix(es)?/' } {  # Matches both 'hotfix/' and 'hotfixes/'
-            $BranchTagName = ($_ -split '/')[-1]  # Extract the leaf name
-            $Config.branches.hotfix.tag = $BranchTagName
-            Write-Host "Hotfix branch detected. Setting the pre-release tag to: $BranchTagName"
-        }
-        default {
-            Write-Host "GitVersion.yml does not need modification for branch: $BranchName. No changes were made."
-            return
-        }
+    }
+
+    if (-not $Matched) {
+        Write-Host "GitVersion.yml does not need modification for branch: $BranchName. No changes were made."
+        return
     }
 
     # Save the updated configuration
