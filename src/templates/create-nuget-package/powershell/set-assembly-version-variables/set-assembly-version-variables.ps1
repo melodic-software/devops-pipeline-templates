be3daf84094c94b103453dfda3a5b05a7930191b
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2024-03-19 15:51:56 -0500

    Handle rerun failed jobs

--- src/templates/create-nuget-package/powershell/set-assembly-version-variables/set-assembly-version-variables.ps1
+++ src/templates/create-nuget-package/powershell/set-assembly-version-variables/set-assembly-version-variables.ps1
@@ -1,6 +1,7 @@
 param(
     [string]$BuildId,
     [string]$BuildNumber,
+    [string]$OriginalBuildNumber,  # Assuming this is passed into the script
     [string]$SemVer,
     [string]$Major,
     [string]$Minor,
@@ -17,6 +18,7 @@ $MajorMinorPatch = "$Major.$Minor.$Patch"
 
 Write-Host "BuildId: $BuildId"
 Write-Host "BuildNumber: $BuildNumber"
+Write-Host "OriginalBuildNumber: $OriginalBuildNumber"
 Write-Host "SemVer: $SemVer"
 Write-Host "Major: $Major"
 Write-Host "Minor: $Minor"
@@ -25,36 +27,49 @@ Write-Host "MajorMinorPatch: $MajorMinorPatch"
 Write-Host "PreReleaseLabel: $PreReleaseLabel"
 Write-Host "ShortSha: $ShortSha"
 
-# The following assumes that the BuildNumber format is $(Date:yyyyMMdd)$(Rev:.r)
-# Extracting Date and Revision parts
-$DatePart = $BuildNumber.Split('.')[0]
-$RevisionPart = $BuildNumber.Split('.')[-1]
-
-$TwoDigitYear = $DatePart.Substring(2,2)
-$DayOfYear = [DateTime]::ParseExact($DatePart, "yyyyMMdd", [Globalization.CultureInfo]::InvariantCulture).DayOfYear.ToString("D3")
-$SafeBuildId = "$TwoDigitYear$DayOfYear"
-$Revision = [int]$RevisionPart.PadLeft(2, '0')
-
-Write-Host "DatePart: $DatePart"
-Write-Host "TwoDigitYear: $TwoDigitYear"
-Write-Host "DayOfYear: $DayOfYear"
-Write-Host "Safe Build ID: $SafeBuildId"
-Write-Host "RevisionPart: $RevisionPart"
-Write-Host "Revision: $Revision"
-
-$AssemblyVersion = "$Major.0.0.0"
-Write-Host "AssemblyVersion: $AssemblyVersion"
-Write-Host "##vso[task.setvariable variable=AssemblyVersion]$AssemblyVersion"
-
-$FileVersion = "$Major.$Minor.$SafeBuildId.$Revision"
-Write-Host "FileVersion: $FileVersion"
-Write-Host "##vso[task.setvariable variable=FileVersion]$FileVersion"
-
-$InformationalVersion = "$MajorMinorPatch"
-if (-not [string]::IsNullOrEmpty($PreReleaseLabel)) {
-    $InformationalVersion = "$InformationalVersion-$PreReleaseLabel"
+# Determine the effective build number to use.
+$EffectiveBuildNumber = if ($OriginalBuildNumber -match '^(\d{8})\.(\d+)$') { 
+    $OriginalBuildNumber 
+} elseif ($BuildNumber -match '^(\d{8})\.(\d+)$') { 
+    $BuildNumber 
+} else { 
+    $null 
 }
-$InformationalVersion = "$InformationalVersion+$ShortSha"
+
+if ($null -ne $EffectiveBuildNumber) {
+    $DatePart = $EffectiveBuildNumber.Split('.')[0]
+    $RevisionPart = $EffectiveBuildNumber.Split('.')[-1]
+
+    $TwoDigitYear = $DatePart.Substring(2,2)
+    $DayOfYear = [DateTime]::ParseExact($DatePart, "yyyyMMdd", [Globalization.CultureInfo]::InvariantCulture).DayOfYear.ToString("D3")
+    $SafeBuildId = "$TwoDigitYear$DayOfYear"
+    $Revision = [int]$RevisionPart.PadLeft(2, '0')
+
+    Write-Host "DatePart: $DatePart"
+    Write-Host "TwoDigitYear: $TwoDigitYear"
+    Write-Host "DayOfYear: $DayOfYear"
+    Write-Host "Safe Build ID: $SafeBuildId"
+    Write-Host "RevisionPart: $RevisionPart"
+    Write-Host "Revision: $Revision"
+
+    $AssemblyVersion = "$Major.0.0.0"
+    Write-Host "AssemblyVersion: $AssemblyVersion"
+    Write-Host "##vso[task.setvariable variable=AssemblyVersion]$AssemblyVersion"
+
+    $FileVersion = "$Major.$Minor.$SafeBuildId.$Revision"
+    Write-Host "FileVersion: $FileVersion"
+    Write-Host "##vso[task.setvariable variable=FileVersion]$FileVersion"
+
+    $InformationalVersion = "$MajorMinorPatch"
+    if (-not [string]::IsNullOrEmpty($PreReleaseLabel)) {
+        $InformationalVersion = "$InformationalVersion-$PreReleaseLabel"
+    }
+    $InformationalVersion = "$InformationalVersion+$ShortSha"
+} else {
+    Write-Error "BuildNumber is not in the expected format."
+    throw "Invalid BuildNumber format, cannot proceed."
+}
+
 Write-Host "InformationalVersion: $InformationalVersion"
 Write-Host "##vso[task.setvariable variable=InformationalVersion]$InformationalVersion"
 
