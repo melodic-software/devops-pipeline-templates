d97a84bf20ef0828d1602b7931a6445d9afa0c8f
Kyle Sexton <kyle.sexton@melodicsoftware.com>
2024-08-13 12:33:42 -0500

    Update variables (reorder coming)

--- src/templates/shared/powershell/functions/WritePredefinedVariables.ps1
+++ src/templates/shared/powershell/functions/WritePredefinedVariables.ps1
@@ -1,6 +1,7 @@
 <#
 .SYNOPSIS
 Logs the values of a predefined set of environment variables.
+
 .DESCRIPTION
 The `Write-PredefinedEnvironmentVariables` function logs the values of a predefined list of environment variables.
 For each variable, it checks if it's set, and logs the appropriate message:
@@ -8,15 +9,19 @@ For each variable, it checks if it's set, and logs the appropriate message:
 - If the variable is not set, it logs that it's not set.
 - If the variable is set and has a value, it logs the value.
 This function is useful for quickly logging the values of common environment variables available in Azure DevOps pipelines or other CI/CD environments.
+
 .PARAMETER VariableNames
 An array of environment variable names to be logged. If not provided, a default set of common environment variables will be used.
+
 .EXAMPLE
 Write-PredefinedEnvironmentVariables
 Logs the values of the default set of environment variables.
+
 .EXAMPLE
 $CustomVariables = @("CUSTOM_VAR1", "CUSTOM_VAR2")
 Write-PredefinedEnvironmentVariables -VariableNames $CustomVariables
 Logs the values of the specified custom environment variables.
+
 .NOTES
 - This function uses the `Write-EnvironmentVariable` function to log each variable's value.
 - The `Write-EnvironmentVariable` function is sourced automatically from the same directory.
@@ -25,7 +30,9 @@ function Write-PredefinedEnvironmentVariables {
     param (
         [Parameter(Mandatory = $false)]
         [string[]]$VariableNames = @(
+            # Agent Variables
             "AGENT_BUILDDIRECTORY",
+            "AGENT_CONTAINERMAPPING",
             "AGENT_HOMEDIRECTORY",
             "AGENT_ID",
             "AGENT_JOBNAME",
@@ -36,23 +43,35 @@ function Write-PredefinedEnvironmentVariables {
             "AGENT_OSARCHITECTURE",
             "AGENT_TEMPDIRECTORY",
             "AGENT_TOOLSDIRECTORY",
+            "AGENT_VERSION",
             "AGENT_WORKFOLDER",
+            "AGENT_DISABLELOGPLUGIN_TESTFILEPUBLISHER",
+
+            # Build Variables
+            "BUILD_CLEAN" # Deprecated
             "BUILD_ARTIFACTSTAGINGDIRECTORY",
+            "BUILD_BINARIESDIRECTORY",
             "BUILD_BUILDID",
             "BUILD_BUILDNUMBER",
+            "BUILD_BUILDNUMBERFORMAT",
             "BUILD_BUILDURI",
-            "BUILD_BINARIESDIRECTORY",
+            "BUILD_CONTAINERID",
+            "Build.CronSchedule.DisplayName", # TRANSLATE THIS
             "BUILD_DEFINITIONNAME",
             "BUILD_DEFINITIONVERSION",
             "BUILD_QUEUEDBY",
             "BUILD_QUEUEDBYID",
             "BUILD_REASON",
             "BUILD_REPOSITORY_CLEAN",
-            "BUILD_REPOSITORY_LOCALPATH",
+            "BUILD_REPOSITORY_GIT_LFS_SUPPORT",
+            "BUILD_REPOSITORY_GIT_SUBMODULECHECKOUT",
             "BUILD_REPOSITORY_ID",
+            "BUILD_REPOSITORY_LOCALPATH",
             "BUILD_REPOSITORY_NAME",
             "BUILD_REPOSITORY_PROVIDER",
             "BUILD_REPOSITORY_URI",
+            "Build.Repository.Tfvc.Workspace", # TRANSLATE THIS
+            "Build.Repository.Uri", # TRANSLATE THIS
             "BUILD_REQUESTEDFOR",
             "BUILD_REQUESTEDFOREMAIL",
             "BUILD_REQUESTEDFORID",
@@ -60,19 +79,66 @@ function Write-PredefinedEnvironmentVariables {
             "BUILD_SOURCEBRANCHNAME",
             "BUILD_SOURCESDIRECTORY",
             "BUILD_SOURCEVERSION",
+            "BUILD_SOURCEVERSIONAUTHOR",
             "BUILD_SOURCEVERSIONMESSAGE",
             "BUILD_STAGINGDIRECTORY",
+            "Build.Repository.Git.SubmoduleCheckout" # TRANSLATE THIS
+            "Build.SourceTfvcShelveset" # TRANSLATE THIS
+            "BUILD_TRIGGEREDBYBUILDID",
+            "BUILD_TRIGGEREDBYDEFINITIONID",
+            "BUILD_TRIGGEREDBYDEFINITIONNAME",
+            "BUILD_TRIGGEREDBYID",
+            "Build.TriggeredBy.BuildNumber", # TRANSLATE THIS AND ENSURE OTHERS ARE ACCURATE
+            "Build.TriggeredBy.ProjectID", # TRANSLATE THIS
+
+            # Common Variables
             "COMMON_TESTRESULTSDIRECTORY",
             "PIPELINE_WORKSPACE",
+            "TF_BUILD",
+
+            # Release Variables
+            "RELEASE_ARTIFACTS_ASPNETCORE_SNAPSHOT_NAME",
+            "RELEASE_ARTIFACTS_ASPNETCORE_VERSION",
+            "RELEASE_DEPLOYMENT_REQUESTEDFOR",
+            "RELEASE_DEPLOYMENT_REQUESTEDFOREMAIL",
+            "RELEASE_ENVIRONMENTID",
+            "RELEASE_ENVIRONMENTNAME",
+            "RELEASE_ENVIRONMENTURI",
+            "RELEASE_RELEASEDESCRIPTION",
+            "RELEASE_RELEASEID",
+            "RELEASE_RELEASENAME",
+            "RELEASE_RELEASEWEBURL",
+            "RELEASE_REQUESTEDFORID",
+            "RELEASE_REQUESTEDFORMAIL",
+            "RELEASE_REQUESTEDFORNAME",
+            "RELEASE_REQUESTEDFORUNIQUEUSERID",
+            "RELEASE_RELEASEURI",
+            "RELEASE_PRIMARYARTIFACT_ALIAS",
+            "RELEASE_PRIMARYARTIFACT_BUILDID",
+            "RELEASE_PRIMARYARTIFACT_DEFINITIONID",
+            "RELEASE_PRIMARYARTIFACT_DEFINITIONNAME",
+            "RELEASE_PRIMARYARTIFACT_SOURCEBRANCH",
+            "RELEASE_PRIMARYARTIFACT_SOURCEBRANCHNAME",
+            "RELEASE_PRIMARYARTIFACT_SOURCEVERSION",
+
+            # System Variables
+            "SYSTEM_ACCESSTOKEN",
             "SYSTEM_ARTIFACTSDIRECTORY",
             "SYSTEM_COLLECTIONID",
+            "SYSTEM_COLLECTIONURI",
+            "SYSTEM_CULTURE",
+            "SYSTEM_DEBUG",
             "SYSTEM_DEFAULTWORKINGDIRECTORY",
             "SYSTEM_DEFINITIONID",
+            "SYSTEM_DEFINITIONNAME",
+            "SYSTEM_ENABLEPROCESSDUMP",
             "SYSTEM_HOSTTYPE",
             "SYSTEM_JOBATTEMPT",
             "SYSTEM_JOBDISPLAYNAME",
             "SYSTEM_JOBID",
+            "SYSTEM_JOBIDENTITY",
             "SYSTEM_JOBNAME",
+            "SYSTEM_OIDCREQUESTURI",
             "SYSTEM_PHASEATTEMPT",
             "SYSTEM_PHASEDISPLAYNAME",
             "SYSTEM_PHASENAME",
@@ -84,14 +150,38 @@ function Write-PredefinedEnvironmentVariables {
             "SYSTEM_PULLREQUEST_SOURCECOMMITID",
             "SYSTEM_PULLREQUEST_SOURCEREPOSITORYURI",
             "SYSTEM_PULLREQUEST_TARGETBRANCH",
+            "System.PullRequest.targetBranchName", # TRANSLATE THIS
             "SYSTEM_STAGEATTEMPT",
             "SYSTEM_STAGEDISPLAYNAME",
             "SYSTEM_STAGENAME",
+            "SYSTEM_TASKDEFINITIONSURI",
             "SYSTEM_TEAMFOUNDATIONCOLLECTIONURI",
             "SYSTEM_TEAMPROJECT",
             "SYSTEM_TEAMPROJECTID",
             "SYSTEM_TIMELINEID",
-            "TF_BUILD"
+            "SYSTEM_WORKFOLDER",
+
+            # Repository Variables (additional)
+            "BUILD_REPOSITORY_CLEAN",
+            "BUILD_REPOSITORY_GIT_SUBMODULECHECKOUT",
+            "BUILD_REPOSITORY_GIT_LFS_SUPPORT",
+            "BUILD_REPOSITORY_NAME",
+            "BUILD_REPOSITORY_PROVIDER",
+            "BUILD_REPOSITORY_URI",
+            "BUILD_REPOSITORY_LOCALPATH",
+            "BUILD_REPOSITORY_ID",
+            "BUILD_SOURCESDIRECTORY",
+
+            "PIPELINE_WORKSPACE",
+
+            "ENVIRONMENT_NAME",
+            "ENVIRONMENT_ID",
+            "ENVIRONMENT_RESOURCENAME",
+            "ENVIRONMENT_RESOURCEID",
+            "STRATEGY_NAME",
+            "STRATEGY_CYCLENAME",
+
+            "CHECKS.STAGEATTEMPT"
         )
     )
 
